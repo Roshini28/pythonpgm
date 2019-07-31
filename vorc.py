8e306d90f079e83f1785e5b482c34023ca6a7f11
Roshini28 <52270291+Roshini28@users.noreply.github.com>
2019-07-31 10:19:46 +0530

    Update and rename voworcon.py to vorc.py

---
+++ vorc.py
@@ -0,0 +1,6 @@
+char=input()
+vowels=['a','e','i','o','u']
+if char in vowels:
+  print("it is a vowel")
+else:
+  print("invalid")
--- voworcon.py
+++
@@ -1,6 +0,0 @@
-ch=input()
-vowels=['a','e','i','o','u']
-if ch in vowels:
-print("it is a vowel")
-else:
-print("invalid")
