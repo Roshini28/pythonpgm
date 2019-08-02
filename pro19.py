d1a5fc61040833a004d4a42bff03a112418e5b9d
Roshini28 <52270291+Roshini28@users.noreply.github.com>
2019-08-02 13:32:55 +0530

    Create pro19.py

---
+++ pro19.py
@@ -0,0 +1,10 @@
+size8=int(input())
+arr3=[]
+for j in range(size8):
+	dt=input()
+	dt=list(map(int,dt.split(" ")))
+	nt=len(dt)
+	for i in range(nt):
+		arr3.append(dt[j])
+arr3.sort()
+print(*arr3,sep=" ")
