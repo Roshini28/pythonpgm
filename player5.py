dcbb0ec913c2567af1af4212d319ba2bab78d2e7
Roshini28 <52270291+Roshini28@users.noreply.github.com>
2019-08-09 14:03:40 +0530

    Create player5.py

---
+++ player5.py
@@ -0,0 +1,7 @@
+list28=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+list30=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+ros=str(input())
+for m in range(20):
+  if(ros==list28[m]):
+    print(list30[m])
+    break
