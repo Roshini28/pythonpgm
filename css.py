467c5f292e37a016c35dcfa247b012e469e0f541
Roshini28 <52270291+Roshini28@users.noreply.github.com>
2019-09-28 15:15:25 +0530

    Create css.py

---
+++ css.py
@@ -0,0 +1,25 @@
+div{
+    background-color: crimson;
+
+}
+p{
+    background-color: darkgoldenrod;
+    font-size : 20px;
+}
+.apec{
+  background-color: deepskyblue;
+    
+}
+.apec1{
+    width: 150px; 
+    height: 160px;
+    border: 2px; solid color:#FF007F;
+    font-size : 20px;
+}
+.adhi{
+    background-color: chartreuse;
+    height: 160px;
+    border: 2px; solid color:#FF007F;
+    font-size : 20px;
+
+}
