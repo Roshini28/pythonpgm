f3eca00a8d816660e3a8e86b62a26000a70839d5
Roshini28 <52270291+Roshini28@users.noreply.github.com>
2019-09-28 15:16:24 +0530

    Create html.py

---
+++ html.py
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="html.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ITgirls</title>
+    <style>
+    
+    </style>
+    
+</head>
+<body background-color="rose">
+    <div class="apec"> Adhiparasakthi</div>
+    <br>
+    <div class="apec1">Dept of it</div>
+    <button type="button">primaryssss</button>
+    <br>
+    
+    
+</body>
+</html>
